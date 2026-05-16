3f6e470b8fbdbf95e5fd3033989c361e950b39bd
vasco <vmscorrea@gmail.com>
2026-05-16 17:22:05 +0100

    teste

--- src/main/java/pt/iefp/javaee/estruturarprojetojavaee/web/HelloServlet.java
+++ src/main/java/pt/iefp/javaee/estruturarprojetojavaee/web/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Olá turma 25480!";
     }
 //teste brunch
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
